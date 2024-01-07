5ed0a7715d12031834d7464dce788f0991245c73
Anna Szumilas <anna.szumilas0@gmail.com>
2024-01-07 18:10:50 +0100

    Added tests

--- app.py
+++ app.py
@@ -24,6 +24,10 @@ auth = identity.web.Auth(
     client_credential=app.config["CLIENT_SECRET"],
 )
 
+movies_db = [
+    {"id": 1, "title": "Movie 1", "director": "Director 1"},
+    {"id": 2, "title": "Movie 2", "director": "Director 2"}
+]
 
 @app.route("/login")
 def login():
@@ -54,8 +58,56 @@ def index():
         return render_template('config_error.html')
     if not auth.get_user():
         return redirect(url_for("login"))
-    return render_template('index.html', user=auth.get_user(), version=identity.__version__)
+    return render_template('index.html', user=auth.get_user(), version=identity.__version__, movies=movies_db)
 
+@app.route('/movies/<int:movie_id>')
+def get_movie(movie_id):
+    if not auth.get_user():
+        return redirect(url_for("login"))
+    movie = next((m for m in movies_db if m["id"] == movie_id), None)
+    if movie:
+        return render_template('movie_detail.html', movie=movie)
+    else:
+        return "Movie not found", 404
+
+
+@app.route('/add_movie', methods=['POST'])
+def add_movie():
+    if not auth.get_user():
+        return redirect(url_for("login"))
+    title = request.form.get('title')
+    director = request.form.get('director')
+
+    new_movie = {"id": len(movies_db) + 1, "title": title, "director": director}
+    movies_db.append(new_movie)
+
+    return redirect(url_for('index'))
+
+
+@app.route('/update_movie/<int:movie_id>', methods=['POST'])
+def update_movie(movie_id):
+    if not auth.get_user():
+        return redirect(url_for("login"))
+    title = request.form.get('title')
+    director = request.form.get('director')
+
+    movie = next((m for m in movies_db if m["id"] == movie_id), None)
+    if movie:
+        movie['title'] = title
+        movie['director'] = director
+
+    return redirect(url_for('index'))
+
+
+@app.route('/delete_movie/<int:movie_id>')
+def delete_movie(movie_id):
+    if not auth.get_user():
+        return redirect(url_for("login"))
+    movie = next((m for m in movies_db if m["id"] == movie_id), None)
+    if movie:
+        movies_db.remove(movie)
+
+    return redirect(url_for('index'))
 
 @app.route("/call_downstream_api")
 def call_downstream_api():
